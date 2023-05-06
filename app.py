e1795a99255920ebdaf92fccfb0b63c0cd450835
Gabriele Fiacconi <80699087+gfiacconi@users.noreply.github.com>
2023-05-06 16:33:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import os 
+import streamlit as st 
 from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
-from apikey import apikey
-import streamlit as st 
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
-os.environ['OPENAI_API_KEY'] = apikey
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 # Set up navbar
 st.set_page_config(page_title='Properlytics', page_icon=':house:', layout='wide')
