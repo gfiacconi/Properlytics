2fbadaa170cdb38639a2d040c159b5d32f3468b7
gfiacconi <fiaccobi@gmail.com>
2023-05-09 16:22:44 +0200

    prova

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ if choice == 'Home':
     chain = load_qa_chain(OpenAI(), chain_type="stuff")
 
     chart_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    np.random.randn(1000, 2) / [50, 50] + [45.0703, 7.6869],
     columns=['lat', 'lon'])
 
     st.pydeck_chart(pdk.Deck(
